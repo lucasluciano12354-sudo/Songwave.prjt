a34390eccd8c789a2e4c85765cc146bbc43a0abe
Lucas luciano <samuel.ssilva@sptech.school>
2026-06-17 03:21:45 -0300

    atualização Banner

---
+++ script.js
@@ -0,0 +1,56 @@
+const slides = document.querySelectorAll(".slide");
+const dots = document.querySelectorAll(".dot");
+
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+let current = 0;
+
+function atualizarIndicadores() {
+
+    dots.forEach(dot => {
+        dot.classList.remove("active");
+    });
+
+    if(dots[current]){
+        dots[current].classList.add("active");
+    }
+}
+
+function mostrarSlide(index){
+
+    slides.forEach(slide => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+
+    atualizarIndicadores();
+}
+
+function trocarBanner(){
+
+    current++;
+
+    if(current >= slides.length){
+        current = 0;
+    }
+
+    mostrarSlide(current);
+}
+
+// CLIQUE NAS BOLINHAS
+dots.forEach((dot, index) => {
+
+    dot.addEventListener("click", () => {
+
+        current = index;
+        mostrarSlide(current);
+
+    });
+
+});
+
+mostrarSlide(0);
+
+setInterval(trocarBanner, 5000);
